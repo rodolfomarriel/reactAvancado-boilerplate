b12d655dd8c408957e3c848cc648a95123a1e76f
Rodolfo Marriel <rodolfo.29r@gmail.com>
2020-11-03 21:28:06 -0300

    Finalizing configuration boilerplate

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,4 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/components/**/stories.tsx',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs',
-  ],
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
 };
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,21 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
-import { withKnobs, text } from '@storybook/addon-knobs';
 import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
+  args: {
+    title: '',
+    description: '',
+  },
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NestJS e Styled Components',
-    )}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic',
+};
+
+export const Default: Story = (args) => <Main {...args} />;
+//'React Avançado'
+//'Description','TypeScript, ReactJS, NestJS e Styled Components'
